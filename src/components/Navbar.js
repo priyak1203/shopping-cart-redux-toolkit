1d8c4ab4983cc8b8de1ca32db54483c2804de3d3
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-12 16:13:06 +0530

    basic navbar ui setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CartIcon } from '../icons';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>Redux Toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

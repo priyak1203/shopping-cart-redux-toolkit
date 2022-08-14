db1e3242869bc1e711072ee8d98307280b047eae
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-14 12:59:20 +0530

    increase and decrease implementation

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { removeItem } from '../features/cart/cartSlice';
+import { decrease, increase, removeItem } from '../features/cart/cartSlice';
 import { ChevronDown, ChevronUp } from '../icons';
 
 const CartItem = ({ id, title, price, img, amount }) => {
@@ -19,13 +19,25 @@ const CartItem = ({ id, title, price, img, amount }) => {
       </div>
       <div className="btn-container">
         {/* increase amount */}
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => dispatch(increase({ id }))}
+        >
           <ChevronUp />
         </button>
         {/* amount */}
         <p className="amount">{amount}</p>
         {/* decrease amount */}
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
